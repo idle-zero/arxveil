5d2ddc82be3beba54b590fd9a8b8e9b77b34c777
idle-zero <187621212+idle-zero@users.noreply.github.com>
2026-08-17 21:17:18 +0200

    feat(server): update config to include GRPCAddress

--- server/internal/config/config.go
+++ server/internal/config/config.go
@@ -1,20 +1,25 @@
 package config
 
-import "os"
+import (
+	"os"
+)
 
 const (
 	defaultServerAddress = ":8080"
+	defaultGRPCAddress   = ":9090"
 	defaultDatabaseURL   = "postgres://arxveil:arxveil-local-dev@localhost:5432/arxveil?sslmode=disable"
 )
 
 type Config struct {
 	ServerAddress string
+	GRPCAddress   string
 	DatabaseURL   string
 }
 
 func Load() Config {
 	return Config{
 		ServerAddress: envOrDefault("ARXVEIL_SERVER_ADDRESS", defaultServerAddress),
+		GRPCAddress:   envOrDefault("ARXVEIL_SERVER_GRPC_ADDRESS", defaultGRPCAddress),
 		DatabaseURL:   envOrDefault("ARXVEIL_DATABASE_URL", defaultDatabaseURL),
 	}
 }
